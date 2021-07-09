e2fe18b3d1a5a548ad056b1484fea7940dde5d9d
HelloSongi <69304233+HelloSongi@users.noreply.github.com>
2021-07-09 17:45:36 +0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -12,3 +12,37 @@ module.exports = {
     }
   }
 };
+
+
+//uncomment the block below to run it on rinkeby testnet
+//add your wallet mnemonic letters
+// and your infura / or any network provider
+
+/*//1. Initialize `truffle-hdwallet-provider`
+const HDWalletProvider = require("truffle-hdwallet-provider");
+// Set your own mnemonic here
+const mnemonic = "wallet mnemonic";
+
+// Module exports to make this configuration available to Truffle itself
+module.exports = {
+  // Object with configuration for each network
+  networks: {
+    // Configuration for mainnet
+    mainnet: {
+      provider: function () {
+        // Setting the provider with the Infura Rinkeby address and Token
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/........
+      },
+      network_id: "1"
+    },
+    // Configuration for rinkeby network
+    rinkeby: {
+      // Special function to setup the provider
+      provider: function () {
+        // Setting the provider with the Infura Rinkeby address and Token
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/.........")
+      },
+      network_id: 4 //Fill in the `network_id` for the Rinkeby network.
+    }
+  }
+}; */
